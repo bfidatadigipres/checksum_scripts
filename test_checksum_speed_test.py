44d6361c0861e8c575ed4c497111da27b29a8a04
SaabriinMo <saabriinm123@gmail.com>
2025-05-01 13:48:03 +0100

    fixing pre-commits

--- test_checksum_speed_test.py
+++ test_checksum_speed_test.py
@@ -4,30 +4,30 @@ import checksum_speed_test
 
 
 def test_crc_easy():
-    crc_check = checksum_speed_test.crc_4096('sample_file.mkv')
-    assert crc_check == '6ed2a568'
+    crc_check = checksum_speed_test.crc_4096("sample_file.mkv")
+    assert crc_check == "6ed2a568"
 
 
 def test_crc_med():
-    crc_check = checksum_speed_test.crc_65536('/dummy_directory/no_file/')
+    crc_check = checksum_speed_test.crc_65536("/dummy_directory/no_file/")
     assert crc_check is None
 
 
 def test_crc_med2():
-    crc_check = checksum_speed_test.crc_4096('')
+    crc_check = checksum_speed_test.crc_4096("")
     assert crc_check is None
 
 
 def test_md5_easy():
-    md5_check = checksum_speed_test.md5_65536('sample_file.mkv')
-    assert md5_check == '6f56b883a687dfb69266d3ed5299e937'
+    md5_check = checksum_speed_test.md5_65536("sample_file.mkv")
+    assert md5_check == "6f56b883a687dfb69266d3ed5299e937"
 
 
 def test_md5_med():
-    md5_check = checksum_speed_test.md5_4096('/dummy_directory/no_file/')
+    md5_check = checksum_speed_test.md5_4096("/dummy_directory/no_file/")
     assert md5_check is None
 
 
 def test_md5_med2():
-    md5_check = checksum_speed_test.md5_65536('')
+    md5_check = checksum_speed_test.md5_65536("")
     assert md5_check is None
