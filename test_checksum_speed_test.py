f7c21f9c7ea24842902d3360f05757bd25d3adc0
SaabriinMo <saabriinm123@gmail.com>
2025-05-02 16:53:56 +0100

    amending testing data- final - final

--- test_checksum_speed_test.py
+++ test_checksum_speed_test.py
@@ -33,3 +33,8 @@ def test_md5_med():
 def test_md5_med2():
     md5_check = checksum_speed_test.md5_65536(".....")
     assert md5_check is None
+
+
+def test_md5_med3():
+    md5_check = checksum_speed_test.md5_65536("")
+    assert md5_check is None
