2cd33ff2d46caa214693587b12ba881ad805f64a
SaabriinMo <saabriinm123@gmail.com>
2025-05-06 17:55:08 +0100

    adding comment: round 2

--- test_checksum_speed_test.py
+++ test_checksum_speed_test.py
@@ -123,3 +123,8 @@ def test_md5_med5m():
 def test_md5_med5hm():
     md5_check = checksum_speed_test.md5_65536("")
     assert md5_check is None
+
+
+def test_md5___():
+    md5_check = checksum_speed_test.md5_65536("")
+    assert md5_check is None
