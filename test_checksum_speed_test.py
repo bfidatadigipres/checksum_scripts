50ed1c969e387de5a1950fc61930cf3c29748539
SaabriinMo <saabriinm123@gmail.com>
2025-05-06 18:35:28 +0100

    breaking code...

--- test_checksum_speed_test.py
+++ test_checksum_speed_test.py
@@ -28,3 +28,8 @@ def test_md5_easy():
 def test_md5_med():
     md5_check = checksum_speed_test.md5_4096("/dummy_directory/no_/")
     assert md5_check is None
+
+
+def test_md5_med_btoken():
+    md5_check = checksum_speed_test.md5_4096(".....")
+    assert md5_check is None
