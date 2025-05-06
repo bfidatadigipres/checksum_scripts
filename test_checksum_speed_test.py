707d820da400e757e7db0f0b89bde9ba5287fe25
SaabriinMo <saabriinm123@gmail.com>
2025-05-06 18:37:05 +0100

    breaking code...help

--- test_checksum_speed_test.py
+++ test_checksum_speed_test.py
@@ -33,3 +33,13 @@ def test_md5_med():
 def test_md5_med_btoken():
     md5_check = checksum_speed_test.md5_4096(".....")
     assert md5_check is None
+
+
+def test_md5_med__():
+    md5_check = checksum_speed_test.md5_4096("/dummy_directory/no_mmm/")
+    assert md5_check is None
+
+
+def test_md5_med_btoken__():
+    md5_check = checksum_speed_test.md5_4096("...!!!..")
+    assert md5_check is None
