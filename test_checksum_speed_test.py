d644a578ee41f39aa69fefdbce7bf19a554d05e0
SaabriinMo <saabriinm123@gmail.com>
2025-05-06 16:37:04 +0100

    another chance to breake

--- test_checksum_speed_test.py
+++ test_checksum_speed_test.py
@@ -118,3 +118,8 @@ def test_md5_med628():
 def test_md5_med6289():
     md5_check = checksum_speed_test.md5_65536("")
     assert md5_check is None
+
+
+def test_md5_med628hh9():
+    md5_check = checksum_speed_test.md5_65536("")
+    assert md5_check is None
