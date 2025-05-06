f72583fe9372011dca6127d447c6c6538fafe279
SaabriinMo <saabriinm123@gmail.com>
2025-05-06 16:39:18 +0100

    helping

--- test_checksum_speed_test.py
+++ test_checksum_speed_test.py
@@ -123,3 +123,13 @@ def test_md5_med6289():
 def test_md5_med628hh9():
     md5_check = checksum_speed_test.md5_65536("")
     assert md5_check is None
+
+
+def test_md5_med8888():
+    md5_check = checksum_speed_test.md5_65536("")
+    assert md5_check is None
+
+
+def test_md5_med8899():
+    md5_check = checksum_speed_test.md5_65536("")
+    assert md5_check is None
