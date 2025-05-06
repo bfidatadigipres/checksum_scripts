12c67c86832110a908e759c10deb1176f3e6d7b6
SaabriinMo <saabriinm123@gmail.com>
2025-05-06 16:58:56 +0100

    breaking code

--- test_checksum_speed_test.py
+++ test_checksum_speed_test.py
@@ -103,13 +103,3 @@ def test_md5_med62():
 def test_md5_med248():
     md5_check = checksum_speed_test.md5_65536("")
     assert md5_check is None
-
-
-def test_md5_med608():
-    md5_check = checksum_speed_test.md5_65536("")
-    assert md5_check is None
-
-
-def test_md5_med682():
-    md5_check = checksum_speed_test.md5_65536("")
-    assert md5_check is None
