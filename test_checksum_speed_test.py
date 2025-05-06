e76de6506dfbdc3483dde8f92eb331d4f27342ea
SaabriinMo <saabriinm123@gmail.com>
2025-05-06 15:40:11 +0100

    adding new changes: amedning files - helping - helping

--- test_checksum_speed_test.py
+++ test_checksum_speed_test.py
@@ -63,3 +63,8 @@ def test_md5_med7():
 def test_md5_med9():
     md5_check = checksum_speed_test.md5_65536("")
     assert md5_check is None
+
+
+def test_md5_med19():
+    md5_check = checksum_speed_test.md5_65536("")
+    assert md5_check is None
