0365078c6283051b7df39fc1362903928b50acb0
SaabriinMo <saabriinm123@gmail.com>
2025-05-06 16:47:45 +0100

    .....

--- test_checksum_speed_test.py
+++ test_checksum_speed_test.py
@@ -138,3 +138,8 @@ def test_md5_med8899():
 def test_md5_med8999():
     md5_check = checksum_speed_test.md5_65536("")
     assert md5_check is None
+
+
+def test_md5_med9999():
+    md5_check = checksum_speed_test.md5_65536("")
+    assert md5_check is None
